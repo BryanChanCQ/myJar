109de3fee83bd1ce99d0e22e5f5b1ead41ccd5e1
chenzpb <chenzpb@yonyou.com>
2025-03-17 09:48:25 +0800

    feat 将第三方依赖打到jar包pom的plugins提交

--- src/main/java/cn/bryanchan/Main.java
+++ src/main/java/cn/bryanchan/Main.java
@@ -1,7 +1,10 @@
 package cn.bryanchan;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.function.Supplier;
+
+import com.google.common.collect.Lists;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -11,6 +14,7 @@ public class Main {
                 return args[0];
             }
         };
+        List<String> list = Lists.newArrayList();
         String s = supplier.get();
         System.out.println("------------------:" + s);
     }
